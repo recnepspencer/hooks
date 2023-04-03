df345c5b53f1dfccc996890fb65c4366a8ac6289
recnepspencer <goldenspencerh@gmail.com>
2023-04-03 17:11:11 -0600

    learning about useEffect

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 import ClassCounter from './Components/ClassCounter';
 import HookCounter from './Components/HookCounter';
 import HookCounterTwo from './Components/HookCounterTwo';
+import HookCounterThree from './Components/HookCounterThree';
+import HookCounterFour from './Components/HookCounterFour';
+import UseEffectCounterOne from './Components/useEffectCounterOne';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterTwo></HookCounterTwo>
+      <UseEffectCounterOne></UseEffectCounterOne>
+      {/* <HookCounterFour></HookCounterFour> */}
+      {/* <HookCounterTwo></HookCounterTwo>
+      <HookCounterThree></HookCounterThree> */}
       {/* <HookCounter></HookCounter> */}
       {/* <ClassCounter></ClassCounter> */}
     </div>
